7d12a3660854f11d538b664c655f4612c397b739
Lucas Zamora <113631100+lucaszamorapy@users.noreply.github.com>
2024-04-22 21:34:24 -0300

    fix: footer fixed in bottom

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,18 @@ import MoviePage from "./pages/MoviePage";
 
 function App() {
   return (
-    <>
+    <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/searchpage" element={<SearchPage />} />
-          <Route path="/movie/:id" element={<MoviePage />} />
-        </Routes>
+        <main className="AppBody">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/searchpage" element={<SearchPage />} />
+            <Route path="/movie/:id" element={<MoviePage />} />
+          </Routes>
+        </main>
         <Footer />
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 
